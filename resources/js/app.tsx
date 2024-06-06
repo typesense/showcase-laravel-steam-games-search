097bf70bbb9692504e78b2448d18a0fa9427bced
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-06-06 11:24:34 +0300

    feat: setup t3-env

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,11 +1,12 @@
 import './bootstrap';
 import '../css/app.css';
+import { env } from './lib/env';
 
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
